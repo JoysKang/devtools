2d0003db2abdbcf061217871bbe6987a30d3ca03
Joys <licoricepieces@gmail.com>
2025-01-03 20:27:31 +0800

    del dioxus

--- rust_back/src/components/home.rs
+++
@@ -1,8 +0,0 @@
-use dioxus::prelude::*;
-
-#[component]
-pub fn Home() -> Element {
-    rsx! {
-        h2 { "欢迎使用 devtools" }
-    }
-} 
--- rust_back/src/components/timestamp.rs
+++
@@ -1,8 +0,0 @@
-use dioxus::prelude::*;
-
-#[component]
-pub fn Timestamp() -> Element {
-    rsx! {
-        div { "时间戳转换功能开发中..." }
-    }
-} 
